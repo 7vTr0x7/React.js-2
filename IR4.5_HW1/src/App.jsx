736a646a330fac6895fd55f6ed919651d1a75ebe
7vTr0x7 <7vTrox7@gmail.com>
2024-06-21 17:31:52 +0530

    IR4.5_HW1 - ChooseMusicGenre

--- IR4.5_HW1/src/App.jsx
+++ IR4.5_HW1/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ChooseEducation from "./components/ChooseEducation";
+import ChooseMusicGenre from "./components/ChooseMusicGenre";
 
 const App = () => {
   return (
     <div>
       <ChooseEducation />
+      <br />
+      <ChooseMusicGenre />
     </div>
   );
 };
