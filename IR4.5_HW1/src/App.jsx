5a4703dfce05e90209a5021a92aae17519b67f22
7vTr0x7 <7vTrox7@gmail.com>
2024-06-21 17:58:10 +0530

    IR4.5_HW2

--- IR4.5_HW1/src/App.jsx
+++ IR4.5_HW1/src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import ChooseEducation from "./components/ChooseEducation";
-import ChooseMusicGenre from "./components/ChooseMusicGenre";
-import ChoosePaymentMethod from "./components/ChoosePaymentMethod";
-import ChooseWeather from "./components/ChooseWeather";
-import ChooseOccupation from "./components/ChooseOccupation";
 
 const App = () => {
-  return (
-    <div>
-      <ChooseEducation />
-      <br />
-      <ChooseMusicGenre />
-      <br />
-      <ChoosePaymentMethod />
-      <br />
-      <ChooseWeather />
-      <br />
-      <ChooseOccupation />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
