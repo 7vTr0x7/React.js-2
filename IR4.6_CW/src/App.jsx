89472b0500f1dc143405635a07a2e88dc7dcfb88
7vTr0x7 <7vTrox7@gmail.com>
2024-06-24 12:21:55 +0530

    IR4.6_CW - DaySelect

--- IR4.6_CW/src/App.jsx
+++ IR4.6_CW/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ColorSelect from "./components/ColorSelect";
+import DaySelect from "./components/DaySelect";
 
 const App = () => {
   return (
     <div>
       <ColorSelect />
+      <br />
+      <DaySelect />
     </div>
   );
 };
--- IR4.6_CW/src/components/DaySelect.jsx
+++ IR4.6_CW/src/components/DaySelect.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DaySelect = () => {
-  return <div>DaySelect</div>;
+  const [day, setDay] = useState("");
+
+  const daySelectHandler = (e) => {
+    setDay(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="daySelect">Select a day: </label>
+      <select id="daySelect" onChange={daySelectHandler}>
+        <option value="Sunday">Sunday</option>
+        <option value="Monday">Monday</option>
+        <option value="Tuesday">Tuesday</option>
+        <option value="Wednesday">Wednesday</option>
+        <option value="Thursday">Thursday</option>
+        <option value="Friday">Friday</option>
+        <option value="Saturday">Saturday</option>
+      </select>
+      <p>
+        {day &&
+          (day === "Sunday" || day === "Saturday"
+            ? `${day} is a weekend`
+            : `${day} is a weekday`)}
+      </p>
+    </div>
+  );
 };
 
 export default DaySelect;
