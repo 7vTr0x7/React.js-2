76b3802adbe5c3b2d69881619e4bd83cf7f5ec1f
7vTr0x7 <7vTrox7@gmail.com>
2024-06-24 15:49:00 +0530

    IR4.6_HW2 - SelectJobType

--- IR4.6_HW2/src/App.jsx
+++ IR4.6_HW2/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SelectClothingSize from "./components/SelectClothingSize";
+import SelectJobType from "./components/SelectJobType";
 
 const App = () => {
   return (
     <div>
       <SelectClothingSize />
+      <br />
+      <SelectJobType />
     </div>
   );
 };
---
+++ IR4.6_HW2/src/components/SelectJobType.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const SelectJobType = () => {
+  const [jobType, setJobType] = useState("");
+  const [text, setText] = useState("");
+
+  const typeSelectHandler = (e) => {
+    setJobType(e.target.value);
+    if (jobType === "Full-time") {
+      setText("Employment for a standard 40-hours workweek");
+    } else if (jobType === "Part-time") {
+      setText("Employment for  less than a standard workweek");
+    } else {
+      setText("Temporary employment for specific project or period");
+    }
+  };
+
+  return (
+    <div>
+      <label>Select a job type: </label>
+      <select id="jobSelect" onChange={typeSelectHandler}>
+        <option value="Full-time">Full-time</option>
+        <option value="Part-time">Part-time</option>
+        <option value="Contract">Contract</option>
+      </select>
+      <p>Description: {text && `${jobType} - ${text}`}</p>
+    </div>
+  );
+};
+
+export default SelectJobType;
