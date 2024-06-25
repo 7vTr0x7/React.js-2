9db399a803c32c1e1c5ead73fb7c893216bbe6f8
7vTr0x7 <7vTrox7@gmail.com>
2024-06-25 15:57:46 +0530

    IR_Practice1 - TempCalculator

--- IR_Practice1/src/App.jsx
+++ IR_Practice1/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PosNeg from "./components/PosNeg";
 import UserLogin from "./components/UserLogin";
+import TempCalculator from "./components/TempCalculator";
 
 const App = () => {
   return (
     <div>
       <PosNeg />
       <UserLogin />
+      <TempCalculator />
     </div>
   );
 };
