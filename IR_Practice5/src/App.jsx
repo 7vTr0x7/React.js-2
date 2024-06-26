7c204f650795902d3dfe25af40683b90653965f7
7vTr0x7 <7vTrox7@gmail.com>
2024-06-26 16:45:17 +0530

    IR_Practice5 - MusicPreferenceApp

--- IR_Practice5/src/App.jsx
+++ IR_Practice5/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TravelDestinationApp from "./components/TravelDestinationApp";
 import "./App.css";
+import MusicPreferenceApp from "./components/MusicPreferenceApp";
 
 const App = () => {
   return (
     <div>
       <TravelDestinationApp />
+      <MusicPreferenceApp />
     </div>
   );
 };
