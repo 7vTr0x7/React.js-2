ff094b9dbefd31c2306b03daf4e5ef435c9f5d11
7vTr0x7 <7vTrox7@gmail.com>
2024-06-26 12:35:41 +0530

    IR_Practice2

--- IR_Practice2/src/App.jsx
+++ IR_Practice2/src/App.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [answers, setAnswers] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+
+  const questions = [
+    {
+      id: 1,
+      question: "Favorite Color?",
+      options: ["Red", "Blue", "Green", "Yellow"],
+    },
+    {
+      id: 2,
+      question: "Favorite Animal?",
+      options: ["Dog", "Cat", "Bird", "Fish"],
+    },
+    {
+      id: 3,
+      question: "Favorite Food?",
+      options: ["Pizza", "Burger", "Pasta", "Salad"],
+    },
+  ];
+
+  const questionsListing = questions.map((que) => (
+    <div key={que.id}>
+      <h2>{que.question}</h2>
+      <ul>
+        {que.options.map((option) => (
+          <li key={option}>
+            <input
+              type="radio"
+              name={que.question}
+              value={option}
+              onChange={(e) => setAnswers((prev) => [...prev, e.target.value])}
+            />
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (answers) {
+      setSubmitted(true);console.log(answers)
+    }
+  };
+
+  return (
+    <div>
+      <h1>Polling App</h1>
+      {submitted ? (
+        <h2>Thank you for participating!</h2>
+      ) : (
+        <form onSubmit={submitHandler}>
+          {questionsListing}
+          <br />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default App;
