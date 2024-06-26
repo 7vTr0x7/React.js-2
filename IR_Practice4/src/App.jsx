a9a31b255ba427eb5534bc3f6b26c3f4696edd99
7vTr0x7 <7vTrox7@gmail.com>
2024-06-26 15:45:35 +0530

    IR_Practice4 - FavoriteMovie

--- IR_Practice4/src/App.jsx
+++ IR_Practice4/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FeedbackApp from "./components/FeedbackApp";
+import FavoriteMovie from "./components/FavoriteMovie";
 
 const App = () => {
   return (
     <div>
       <FeedbackApp />
+      <FavoriteMovie />
     </div>
   );
 };
