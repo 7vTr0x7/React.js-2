86d80a52909a4f5981b7ac54c37fa1abdb7724bb
7vTr0x7 <7vTrox7@gmail.com>
2024-06-27 16:06:49 +0530

    IR_HW2 - Biography

--- IR_HW2/src/pages/Biography.jsx
+++ IR_HW2/src/pages/Biography.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Biography = () => {
-  return <div>Biography</div>;
+  return (
+    <>
+      <Header />
+      <main className="container py-4">
+        <h1>Biography</h1>
+        <p>
+          Virat Kohli, born on November 5, 1988, in Delhi, India, is a
+          celebrated cricketer known for his aggressive batting and leadership
+          skills. Rising through the ranks of youth cricket, he made his
+          international debut in 2008 and has since become one of the most
+          successful and influential players in the sport.
+        </p>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Biography;
