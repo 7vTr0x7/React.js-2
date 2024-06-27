030326ad97d49795fefd460819d0db6098cc4292
7vTr0x7 <7vTrox7@gmail.com>
2024-06-27 15:16:54 +0530

    IR_HW1 - Routes

--- IR_HW1/src/components/Header.jsx
+++ IR_HW1/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,9 +9,21 @@ const Header = () => {
 
         <nav className="pt-4">
           <ul className="nav">
-            <li className="nav-item">Overview</li>
-            <li className="nav-item">History</li>
-            <li className="nav-item">Architecture</li>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                Overview
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/history" className="nav-link">
+                History
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/architecture" className="nav-link">
+                Architecture
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
