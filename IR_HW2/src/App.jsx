3aa42664d9ddc553342f5589174bbcb8a9aef1c1
7vTr0x7 <7vTrox7@gmail.com>
2024-06-27 15:43:55 +0530

    IR_HW2 - overview

--- IR_HW2/src/App.jsx
+++ IR_HW2/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Overview from "./pages/Overview";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Overview />
+    </>
+  );
+};
 
-export default App
+export default App;
