c2251d5df2fdb1968a994fa050a4afb1bbb38ceb
7vTr0x7 <7vTrox7@gmail.com>
2024-06-27 15:51:59 +0530

    IR_HW2 - Footer

---
+++ IR_HW2/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light">
+      <div className="container py-3">
+        <p>&copy; 2024 Virat Kolhi Fan Club</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
