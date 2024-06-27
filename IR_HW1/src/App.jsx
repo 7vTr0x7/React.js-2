da85a01b4ba5dd03fa6db85df79187c5cb911822
7vTr0x7 <7vTrox7@gmail.com>
2024-06-27 15:15:20 +0530

    IR_HW1 - Routes

--- IR_HW1/src/App.jsx
+++ IR_HW1/src/App.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
 const App = () => {
   return (
     <div>
--- IR_HW1/src/main.jsx
+++ IR_HW1/src/main.jsx
@@ -2,8 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import History from "./pages/History";
+import Architecture from "./pages/Architecture";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/history",
+    element: <History />,
+  },
+  {
+    path: "/architecture",
+    element: <Architecture />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
