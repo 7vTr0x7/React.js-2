84315bbe674f8dd1f4defa95d7a4acda28a64e54
7vTr0x7 <7vTrox7@gmail.com>
2024-06-27 15:57:00 +0530

    IR_HW2 - overview

--- IR_HW2/src/pages/Overview.jsx
+++ IR_HW2/src/pages/Overview.jsx
@@ -6,7 +6,30 @@ const Overview = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main className="container py-4">
+        <h1>Overview</h1>
+        <div>
+          <img
+            alt="virat"
+            className="img-fluid"
+            src="https://www.hindustantimes.com/static-content/1y/cricket-logos/players/virat-kohli.png"
+          />
+        </div>
+        <p className="pt-4">
+          Virat Kohli is an Indian cricketer known for his exceptional batting
+          skills and leadership, serving as the former captain of the Indian
+          national team. Renowned for his aggressive play and numerous records,
+          he is one of the most successful and influential cricketers in modern
+          cricket.
+        </p>
+        <p>
+          Virat Kohli is a renowned Indian cricketer celebrated for his prolific
+          run-scoring and dynamic leadership, having captained the Indian
+          national team across all formats. He is widely regarded as one of the
+          best batsmen of his generation, with numerous records and accolades to
+          his name.
+        </p>
+      </main>
       <Footer />
     </>
   );
