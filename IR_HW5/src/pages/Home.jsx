0697161af5f89d055ea6cfc31f8d5f1adffb54aa
7vTr0x7 <7vTrox7@gmail.com>
2024-06-28 15:07:45 +0530

    IR_HW5 - Home

--- IR_HW5/src/pages/Home.jsx
+++ IR_HW5/src/pages/Home.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main className="container py-5">
+        <div>
+          <img
+            className="img-fluid"
+            alt="Player Playing"
+            src="https://placehold.co/500x300?text=Player Playing"
+          />
+        </div>
+        <div className="pt-4">
+          <h2>Our Players</h2>
+          <p>
+            Meet our talented team of players who work hard to achieve success
+            on field.
+          </p>
+          <Link to="/players" className="btn btn-primary">
+            View Players
+          </Link>
+        </div>
+        <div className="pt-4">
+          <h2>Team Performance</h2>
+          <p>
+            Explore our team&apos;s performance statistics to gain insights into
+            our success on field.
+          </p>
+          <Link to="/players" className="btn btn-primary">
+            View Performance
+          </Link>
+        </div>
+      </main>
       <Footer />
     </>
   );
