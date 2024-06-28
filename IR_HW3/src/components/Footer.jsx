25e4d09bfd85b091897f052d5a5ed83ce851958b
7vTr0x7 <7vTrox7@gmail.com>
2024-06-28 11:02:24 +0530

    IR_HW3 - Footer

---
+++ IR_HW3/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light">
+      <div className="container py-3">
+        <p>&copy; 2024 Apple Inc</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
