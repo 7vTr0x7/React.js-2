49988dd200708ed999a6edcdd0cb2ebd17c1c52d
7vTr0x7 <7vTrox7@gmail.com>
2024-06-28 12:42:51 +0530

    IR_HW4 - Details

---
+++ IR_HW4/src/pages/Details.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Details = () => {
+  return (
+    <>
+      <Header />
+      <main className="container py-4"></main>
+    </>
+  );
+};
+
+export default Details;
