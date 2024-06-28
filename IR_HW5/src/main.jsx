f12eca27c5e60c7af4a8f4b4c0f9c281fe1b8477
7vTr0x7 <7vTrox7@gmail.com>
2024-06-28 14:56:03 +0530

    IR_HW5 - Routes

--- IR_HW5/src/main.jsx
+++ IR_HW5/src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Players from "./pages/Players.jsx";
+import Report from "./pages/Report.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/players",
+    element: <Players />,
+  },
+  {
+    path: "/report",
+    element: <Report />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
