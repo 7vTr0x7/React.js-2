93f3a26ea8a7c8b1ad210a2d6711f30041c25e73
7vTr0x7 <7vTrox7@gmail.com>
2024-06-28 11:25:57 +0530

    IR_HW3 - Overview

--- IR_HW3/src/pages/Overview.jsx
+++ IR_HW3/src/pages/Overview.jsx
@@ -6,7 +6,21 @@ const Overview = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main className="container py-4">
+        <h1>Overview</h1>
+        <div>
+          <img
+            alt="iPhone 15"
+            className="img-fluid"
+            src="https://www.apple.com/newsroom/images/2023/09/apple-unveils-iphone-15-pro-and-iphone-15-pro-max/article/Apple-iPhone-15-Pro-lineup-hero-230912_Full-Bleed-Image.jpg.large.jpg"
+          />
+        </div>
+        <p className="pt-3 fw-normal">
+          Introducing the all-new iPhone 15, the latest innovation form apple.
+          Explore its incredible features, specifications, and stunning design
+          in this website.
+        </p>
+      </main>
       <Footer />
     </>
   );
