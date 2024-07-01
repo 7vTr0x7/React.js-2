44f5ccc057cf1da7c11001c8239e3ab7eab1b503
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 12:10:37 +0530

    IR5.1_HW1 - Home

--- IR5.1_HW1/src/pages/Home.jsx
+++ IR5.1_HW1/src/pages/Home.jsx
@@ -6,6 +6,32 @@ const Home = () => {
   return (
     <>
       <Header />
+      <div className="container py-4">
+        <div className="py-3">
+          <img
+            src="https://placehold.co/400x300?text=Employee+Smiling"
+            alt="Employee"
+            className="img-fluid"
+          />
+        </div>
+
+        <div className="py-4">
+          <h2>Our Employees</h2>
+          <p>
+            Meet our dedicated team of professionals who work tirelessly to
+            achieve our company&apos;s goals.
+          </p>
+          <a className="btn btn-primary">View Employees</a>
+        </div>
+        <div className="py-4">
+          <h2>Company Report</h2>
+          <p>
+            Explore our latest financial report to gain insights into our
+            company&apos;s performance and growth.
+          </p>
+          <a className="btn btn-primary">View Report</a>
+        </div>
+      </div>
       <Footer />
     </>
   );
