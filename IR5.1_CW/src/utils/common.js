63206cc193000fe96fbffb225f69b9eb2c4ab918
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 10:48:26 +0530

    IR5.1_CW - Movies

---
+++ IR5.1_CW/src/utils/common.js
@@ -0,0 +1,27 @@
+export const moviesData = [
+  { id: 1, title: "Inception", genre: "Sci-Fi", director: "Christopher Nolan" },
+  {
+    id: 2,
+    title: "The Shawshank Redemption",
+    genre: "Drama",
+    director: "Frank Darabont",
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    genre: "Crime",
+    director: "Quentin Tarantino",
+  },
+  {
+    id: 4,
+    title: "The Godfather",
+    genre: "Crime",
+    director: "Francis Ford Coppola",
+  },
+  {
+    id: 5,
+    title: "The Dark Knight",
+    genre: "Action",
+    director: "Christopher Nolan",
+  },
+];
