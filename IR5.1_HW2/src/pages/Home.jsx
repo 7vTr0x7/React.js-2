aeedbc50055651a854ccf765f439b8dd013ef982
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 15:24:50 +0530

    IR5.1_HW2 - Home

--- IR5.1_HW2/src/pages/Home.jsx
+++ IR5.1_HW2/src/pages/Home.jsx
@@ -6,7 +6,28 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div></div>
+      <div className="container py-4">
+        <div>
+          <img
+            src="https://placehold.co/400x300?text=Healthy+Lifestyle"
+            className="img-fluid"
+            alt="Healthy Lifestyle"
+          />
+        </div>
+        <div className="mt-4">
+          <h2>Health Resources</h2>
+          <p>
+            Explore food and information to support your journey towards a
+            healthier lifestyle.
+          </p>
+          <a className="btn btn-primary">Explore Food Items</a>
+        </div>
+        <div className="mt-4">
+          <h2>Health Report</h2>
+          <p>See health report</p>
+          <a className="btn btn-primary">View Report</a>
+        </div>
+      </div>
       <Footer />
     </>
   );
