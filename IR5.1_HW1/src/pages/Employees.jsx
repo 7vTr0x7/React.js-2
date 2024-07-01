4fdac60248119b58abb29347f9b55547048a4d9d
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 12:25:53 +0530

    IR5.1_HW1 - Employees

--- IR5.1_HW1/src/pages/Employees.jsx
+++ IR5.1_HW1/src/pages/Employees.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Employees = () => {
-  return <div></div>;
+  const [department, setDepartment] = useState("");
+
+  return (
+    <>
+      <Header />
+      <div className="container py-4">
+        <h1>List of Employees</h1>
+        <div className="py-4">
+          <label id="departmentFilter">Filter By Department: </label>
+          <select
+            id="departmentFilter"
+            onChange={(e) => setDepartment(e.target.value)}
+            className="form-select">
+            <option value="All">All Department</option>
+            <option value="IT">IT</option>
+            <option value="Design">Design</option>
+            <option value="Management">Management</option>
+          </select>
+        </div>
+
+        <div>
+          <ul className="list-group">{}</ul>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Employees;
