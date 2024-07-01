bc4eaba0273607614a972de4685baf7d8e4b28b2
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 09:55:02 +0530

    IR5.1_CW - changes

--- IR5.1_CW/src/pages/Home.jsx
+++ IR5.1_CW/src/pages/Home.jsx
@@ -1,36 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link " href="#">
-                  Home
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+  return <h1></h1>;
 };
 
 export default Home;
