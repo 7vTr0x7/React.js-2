5bd7fefaef3afaf48edf1152665142bebcbe71c8
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 18:48:37 +0530

    IR5.3_CW

--- IR5.3_CW/src/App.jsx
+++ IR5.3_CW/src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import useFetch from "./hooks/useFetch";
 
 const App = () => {
+  const { data, loading, error } = useFetch(
+    "https://randomuser.me/api/?results=3"
+  );
+
   return (
     <div>
       <h1>Random users</h1>
+
+      <div>
+        {loading && <p>Loading...</p>}
+        {error && <p>En error ocurred while fetching users.</p>}
+        {data && data.results && data.results.length > 0 ? (
+          data.results.map((user) => (
+            <div key={user}>
+              <img src={user.picture.medium} alt="User Image" />
+              <p>
+                Name: {user.name.first} {user.name.last}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>User not found</p>
+        )}
+      </div>
     </div>
   );
 };
