0e5d43e73eda20f7b868dfa94a6260f1083dea21
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 11:11:03 +0530

    IR5.1_CW - changes

--- IR5.1_CW/src/pages/Report.jsx
+++ IR5.1_CW/src/pages/Report.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { moviesData } from "../utils/common";
 
 const Report = () => {
-  return <div></div>;
+  const mostWatchedMovie = moviesData.reduce((acc, curr) =>
+    acc.view < curr.view ? (acc = curr) : acc
+  );
+
+  console.log(mostWatchedMovie);
+
+  return (
+    <>
+      <Header />
+      <div className="container py-4">
+        <h2>Movie Report</h2>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Report;
--- IR5.1_CW/src/utils/common.js
+++ IR5.1_CW/src/utils/common.js
@@ -1,31 +1,37 @@
 export const moviesData = [
-  { id: 1, title: "Inception", genre: "Sci-Fi", director: "Christopher Nolan" },
+  {
+    id: 1,
+    title: "Inception",
+    genre: "Sci-Fi",
+    director: "Christopher Nolan",
+    views: 2000,
+  },
   {
     id: 2,
     title: "The Shawshank Redemption",
     genre: "Drama",
     director: "Frank Darabont",
-    view: 2000,
+    views: 2000,
   },
   {
     id: 3,
     title: "Pulp Fiction",
     genre: "Crime",
     director: "Quentin Tarantino",
-    view: 4000,
+    views: 4000,
   },
   {
     id: 4,
     title: "The Godfather",
     genre: "Crime",
     director: "Francis Ford Coppola",
-    view: 1000,
+    views: 1000,
   },
   {
     id: 5,
     title: "The Dark Knight",
     genre: "Action",
     director: "Christopher Nolan",
-    view: 5000,
+    views: 5000,
   },
 ];
