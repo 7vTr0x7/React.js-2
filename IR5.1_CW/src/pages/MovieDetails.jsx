5915ccdd851e5fd77544f176f38054af3589e7fe
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 11:01:23 +0530

    IR5.1_CW - MovieDetails

--- IR5.1_CW/src/pages/MovieDetails.jsx
+++ IR5.1_CW/src/pages/MovieDetails.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { moviesData } from "../utils/common";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const MovieDetails = () => {
   const movieId = useParams();
@@ -8,7 +10,29 @@ const MovieDetails = () => {
   const movie = moviesData.find((movie) => movie.id == movieId.movieId);
   console.log(movie);
 
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main className="container py-4">
+        <h1>{movie.title}</h1>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <b>Title: </b>
+            {movie.title}
+          </li>
+          <li className="list-group-item">
+            <b>Genre: </b>
+            {movie.genre}
+          </li>
+          <li className="list-group-item">
+            <b>Director: </b>
+            {movie.director}
+          </li>
+        </ul>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default MovieDetails;
