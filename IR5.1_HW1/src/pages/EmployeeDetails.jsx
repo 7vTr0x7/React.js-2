1ad8733c5b6fe83e130dd4a93fe652858a855680
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 12:38:05 +0530

    IR5.1_HW1 - Employees

--- IR5.1_HW1/src/pages/EmployeeDetails.jsx
+++ IR5.1_HW1/src/pages/EmployeeDetails.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { employees } from "../utils/common";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const EmployeeDetails = () => {
-  return <div></div>;
+  const emp = useParams();
+
+  const employee = employees.find((employee) => employee.id == emp.empId);
+
+  return (
+    <>
+      <Header />
+
+      <div className="container py-4">
+        <h1>{employee.name}</h1>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <b>Name: </b>
+            {employee.name}
+          </li>
+          <li className="list-group-item">
+            <b>Designation: </b>
+            {employee.title}
+          </li>
+          <li className="list-group-item">
+            <b>Department: </b>
+            {employee.department}
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default EmployeeDetails;
