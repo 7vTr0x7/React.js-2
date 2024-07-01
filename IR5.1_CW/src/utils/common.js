74718dae95de8e86180d45351b08911df22f5144
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 11:06:13 +0530

    IR5.1_CW - utils

--- IR5.1_CW/src/utils/common.js
+++ IR5.1_CW/src/utils/common.js
@@ -5,23 +5,27 @@ export const moviesData = [
     title: "The Shawshank Redemption",
     genre: "Drama",
     director: "Frank Darabont",
+    view: 2000,
   },
   {
     id: 3,
     title: "Pulp Fiction",
     genre: "Crime",
     director: "Quentin Tarantino",
+    view: 4000,
   },
   {
     id: 4,
     title: "The Godfather",
     genre: "Crime",
     director: "Francis Ford Coppola",
+    view: 1000,
   },
   {
     id: 5,
     title: "The Dark Knight",
     genre: "Action",
     director: "Christopher Nolan",
+    view: 5000,
   },
 ];
