2c14425e4dfb75124a5f22f5fd92d1610fb8fcff
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 12:16:17 +0530

    IR5.1_HW1 - Routes

--- IR5.1_HW1/src/main.jsx
+++ IR5.1_HW1/src/main.jsx
@@ -3,9 +3,27 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Employees from "./pages/Employees.jsx";
+import Report from "./pages/Report.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/employees",
+    element: <Employees />,
+  },
+  {
+    path: "/report",
+    element: <Report />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
