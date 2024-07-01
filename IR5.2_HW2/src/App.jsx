04ccb4b9b4be5d2ee27b544d662f747bfb55b451
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 18:21:13 +0530

    IR5.2_HW2

--- IR5.2_HW2/src/App.jsx
+++ IR5.2_HW2/src/App.jsx
@@ -4,16 +4,18 @@ import useFetch from "./hooks/useFetch";
 const App = () => {
   const [showData, setShowData] = useState(false);
 
-  const { data } = useFetch("https://jsonplaceholder.typicode.com/todos/4");
+  const { data } = useFetch(
+    "https://v2.jokeapi.dev/joke/Programming?type=single"
+  );
 
   return (
     <div>
       <h1>Programming API</h1>
-      <button onClick={() => setShowData(true)}>Get Data</button>
+      <button onClick={() => setShowData(true)}>Get Programming Joke</button>
       {showData && (
         <div>
           {data ? (
-            <p>{JSON.stringify(data)}</p>
+            <p>{JSON.stringify(data.joke)}</p>
           ) : (
             <p>An Error ocurred while fetching joke.</p>
           )}
