4463f1464f95ffaaca3fd2eb1236dfef4bbfd718
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 15:38:33 +0530

    IR5.1_HW2 - utils

---
+++ IR5.1_HW2/src/utils/common.js
@@ -0,0 +1,7 @@
+export const foodItemsData = [
+  { id: 1, name: "Apple", calories: 52, category: "Fruit" },
+  { id: 2, name: "Banana", calories: 89, category: "Fruit" },
+  { id: 3, name: "Chicken Breast", calories: 165, category: "Protein" },
+  { id: 4, name: "Salad", calories: 100, category: "Vegetable" },
+  { id: 5, name: "Yogurt", calories: 120, category: "Dairy" },
+];
