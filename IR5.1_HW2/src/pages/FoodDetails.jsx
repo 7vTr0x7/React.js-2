cc3b16933c8160dea545496bc0596f6b6c678bff
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 17:23:35 +0530

    IR5.1_HW2 - FoodDetails

---
+++ IR5.1_HW2/src/pages/FoodDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const FoodDetails = () => {
+  return (
+    <>
+      <Header />
+      <div className="container py-4"></div>
+      <Footer />
+    </>
+  );
+};
+
+export default FoodDetails;
