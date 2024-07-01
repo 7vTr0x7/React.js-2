e04b71fd2cb531e3026300f1dcafe4f4eda0a08c
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 18:04:30 +0530

    IR5.2_CW

--- IR5.2_CW/src/App.jsx
+++ IR5.2_CW/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useState } from "react";
+import useFetch from "./hooks/useFetch";
 
 const App = () => {
-  return <div></div>;
+  const [showData, setShowData] = useState(false);
+
+  const { data } = useFetch("https://jsonplaceholder.typicode.com/todos/1");
+
+  return (
+    <div>
+      <h1>Todo API</h1>
+      <button onClick={() => setShowData(true)}>Get Todo</button>
+      {showData && (
+        <div>
+          {data ? <p>{JSON.stringify(data)}</p> : <p>Unable to fetch data</p>}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
