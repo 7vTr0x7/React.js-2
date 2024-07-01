322468eae62b5d77a22090764c26d0c5d15983c5
7vTr0x7 <7vTrox7@gmail.com>
2024-07-01 10:54:48 +0530

    IR5.1_CW - MovieDetails

--- IR5.1_CW/src/pages/MovieDetails.jsx
+++ IR5.1_CW/src/pages/MovieDetails.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { moviesData } from "../utils/common";
 
 const MovieDetails = () => {
   const movieId = useParams();
+
+  const movie = moviesData.find((movie) => movie.id == movieId.movieId);
+  console.log(movie);
 
   return <div></div>;
 };
