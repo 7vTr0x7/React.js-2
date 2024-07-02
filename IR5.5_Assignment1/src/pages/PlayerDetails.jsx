c5fd00fa0f8e7d5c75ca6094580110691a72892a
7vTr0x7 <7vTrox7@gmail.com>
2024-07-02 12:23:31 +0530

    IR5_Assignment1 - PlayerDetails

--- IR5.5_Assignment1/src/pages/PlayerDetails.jsx
+++ IR5.5_Assignment1/src/pages/PlayerDetails.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useParams } from "react-router-dom";
+import { playersData } from "../utils/common";
 
 const PlayerDetails = () => {
-  return <></>;
+  const p = useParams();
+
+  const selectedPlayer = playersData.find((player) => player.id == p.playerId);
+
+  return (
+    <>
+      <Header />
+      <div className="container py-4">
+        <h2>{selectedPlayer.name}</h2>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <b>Name: </b> {selectedPlayer.name}
+          </li>
+          <li className="list-group-item">
+            <b>Position: </b> {selectedPlayer.position}
+          </li>
+          <li className="list-group-item">
+            <b>Team: </b>
+            {selectedPlayer.team}{" "}
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default PlayerDetails;
