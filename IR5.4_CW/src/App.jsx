2a1ff8d143430cc541b1cd3ab4abfa627f512cca
7vTr0x7 <7vTrox7@gmail.com>
2024-07-02 10:52:05 +0530

    IR5.4_CW

--- IR5.4_CW/src/App.jsx
+++ IR5.4_CW/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import useFetch from "./hooks/useFetch";
 
 const App = () => {
-  return <div></div>;
+  const { data, loading, error, fetchData } = useFetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return (
+    <div className="container py-4">
+      <h1>Fetch Data on Button Click</h1>
+      <button onClick={fetchData}>Fetch Data</button>
+      {loading && <p>Loading...</p>}
+      {error && <p>An error occurred while fetching posts.</p>}
+      <ul>{data && data.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+    </div>
+  );
 };
 
 export default App;
