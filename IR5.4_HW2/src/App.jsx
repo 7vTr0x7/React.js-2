881767062d7477971fbba894c8d782da55ed8076
7vTr0x7 <7vTrox7@gmail.com>
2024-07-02 11:21:40 +0530

    IR5.4_HW2

--- IR5.4_HW2/src/App.jsx
+++ IR5.4_HW2/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import useFetch from "./hooks/useFetch";
 
 const App = () => {
-  return <div></div>;
+  const { data, loading, error, fetchData } = useFetch(
+    "https://api.adviceslip.com/advice"
+  );
+
+  console.log(data);
+
+  return (
+    <div className="container text-center py-4">
+      <h1>Advice Slip API</h1>
+      <button onClick={fetchData} className="btn btn-primary">
+        Get Advice
+      </button>
+      <div className="my-3">
+        {loading && <p>Loading...</p>}
+        {error && <p>Error occurred while fetching advice.</p>}
+        {data && (
+          <p>
+            <b>{data.slip.advice}</b>
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default App;
