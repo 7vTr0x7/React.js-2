d6eb61d8819a0e4605aa55fe65f1f78730d7e3d0
7vTr0x7 <7vTrox7@gmail.com>
2024-07-02 12:39:31 +0530

    IR5_Assignment2

--- IR5.5_Assignment2/src/App.jsx
+++ IR5.5_Assignment2/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="container py-4">
+      <h1>People Directory</h1>
+      <button className="btn btn-primary">Get People</button>
+    </div>
+  );
 };
 
 export default App;
