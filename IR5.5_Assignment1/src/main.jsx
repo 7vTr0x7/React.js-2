5be4f6eca66f99d119e8c585dfdb74d72a65ba6b
7vTr0x7 <7vTrox7@gmail.com>
2024-07-02 11:47:50 +0530

    IR5_Assignment1 - Routes

--- IR5.5_Assignment1/src/main.jsx
+++ IR5.5_Assignment1/src/main.jsx
@@ -3,9 +3,27 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Players from "./pages/Players";
+import Report from "./pages/Report";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/players",
+    element: <Players />,
+  },
+  {
+    path: "/report",
+    element: <Report />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
